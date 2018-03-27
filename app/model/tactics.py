0e0f4215541b64dd2393aec84706c54d070b4f8c
zwkatgithub <zhangwenkai2013@163.com>
2018-03-27 17:02:07 +0800

    Update tactics.py

--- app/model/tactics.py
+++ app/model/tactics.py
@@ -1,11 +1,4 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:123456@127.0.0.1:3306/strategy"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-
-db = SQLAlchemy(app)
+from app import db
 
 class Strategy(db.modle):
     __tablename__ = "strategy"
@@ -19,4 +12,4 @@ class Strategy(db.modle):
     v_pg = db.Column(db.Interger)
     v_sf = db.Column(db.Interger)
     v_pf = db.Column(db.Interger)
-    v_c = db.Column(db.Interger)
+    v_c = db.Column(db.Interger)from app import db
