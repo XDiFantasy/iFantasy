28de156689d8c1b26467e060d1ff66a2093e42ec
zwkatgithub <zhangwenkai2013@163.com>
2018-03-28 09:55:11 +0800

    Update activity.py

--- app/model/activity.py
+++ app/model/activity.py
@@ -1,50 +1,43 @@
 from app import db
 
-#
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost/nba'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
-
-
 class VipCard(db.Model):
     __tablename__ = 'vip_card'
-    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=0)
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     time = db.Column(db.Integer, default=0, nullable=True)
     price = db.Column(db.Integer, default=0, nullable=True)
 
-    def __init__(self, id, time, price):
-        self.id = id
+    def __init__(self, time, price):
         self.time = time
         self.price = price
 
     def __repr__(self):
-        return '<User %r>' % self.id
+        return '<VipCard %r>' % self.id
 
 
 class Theme(db.Model):
     __tablename__ = 'theme'
-    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=0)
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     title = db.Column(db.String(255), nullable=True)
     detail = db.Column(db.String(255), nullable=True)
     price = db.Column(db.Integer, nullable=True)
 
-    def __init__(self, id, title, detail, price):
-        self.id = id
+    def __init__(self, title, detail, price):
         self.title = title
         self.detail = detail
         self.price = price
 
     def __repr__(self):
-        return '<User %r>' % self.id
+        return '<Theme %r>' % self.id
 
 
 class Vip(db.Model):
     __tablename__ = 'vip'
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
     level = db.Column(db.Integer, nullable=True, default=0)
     active = db.Column(db.Boolean, nullable=True, default=0)
     duedate = db.Column(db.DateTime, nullable=True)
+    
+    user = db.relationship('User',backref='vip',lazy='dynamic')
 
     def __init__(self, user_id, level, active, duedate):
         self.user_id = user_id
@@ -53,43 +46,37 @@ class Vip(db.Model):
         self.duedate = duedate
 
     def __repr__(self):
-        return '<User %r>' % self.user_id
+        return '<Vip %r>' % self.user_id
 
 
 class FundType(db.Model):
     __tablename__ = 'fund_type'
-    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=0)
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     price = db.Column(db.Integer, nullable=True, default=0)
     rate = db.Column(db.Float, nullable=True, default=1)
 
-    def __init__(self, id, price, rate):
-        self.id = id
+    def __init__(self,price, rate):
         self.price = price
         self.rate = rate
 
     def __repr__(self):
-        return '<User %r>' % self.id
+        return '<FundType %r>' % self.id
 
 
 class Fund(db.Model):
     __tablename__ = 'fund'
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True, nullable=False)
-    fund_type_id = db.Column(db.Integer, db.ForeignKey('fund_type.id'), primary_key=True, nullable=False)
+    __table_args__ = (
+        db.PrimaryKeyConstraint('user_id', 'fund_type_id'),
+    )
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    fund_type_id = db.Column(db.Integer, db.ForeignKey('fund_type.id'))
+    
+    user  = db.relationship('User',backref='fund',lazy='dynamic')
+    fund_type = db.relationship('FundType', backref='fund', lazy='dynamic')
 
     def __init__(self, user_id, fund_type_id):
         self.user_id = user_id
         self.fund_type_id = fund_type_id
 
     def __repr__(self):
-        return '<User %r>' % self.user_id
-
-
-# class User(db.Model):
-#     __tablename__ = 'user'
-#     id = db.Column(db.Integer, primary_key=True)
-#
-#     def __init__(self, id):
-#         self.id = id
-#
-#     def __repr__(self):
-#         return '<User %r>' % self.id
+        return '<Fund %r, %r>' % (self.user_id, self.fund_type_id)
