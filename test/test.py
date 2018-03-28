d50ef6cc87afa810d8d5923aeb9807f341d1deb8
xd-sonp <sonpking@163.com>
2018-03-28 09:10:38 +0800

    k

--- test/test.py
+++ test/test.py
@@ -1,19 +1,16 @@
-from flask import Flask, Blueprint, request
-from flask_restful import Api, Resource, url_for
-
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/api/v1/login",methods=['POST'])
+
+@app.route("/api/v1/login", methods=['POST'])
 def login():
     phone = request.get_json().get('phone')
     zone = request.get_json().get('zone')
     code = request.get_json().get('code')
-    
-
 
 
 api.add_resource(TodoItem, '/todos/<int:id>')
-app.register_blueprint(api_bp,url_prefix='/api')
+app.register_blueprint(api_bp, url_prefix='/api')
 
 app.run(debug=True)
