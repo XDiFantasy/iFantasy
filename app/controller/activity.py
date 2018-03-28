02332469ef4485419de1c8e800c8f4b24eb1f5a3
zwkatgithub <zhangwenkai2013@163.com>
2018-03-28 08:51:30 +0800

    Update activity.py

--- app/controller/activity.py
+++ app/controller/activity.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from flask_restful import Api
+from flask_restful import Api, Resource
 
 activity_bp = Blueprint('activity_bp', __name__)
 activity_api = Api(activity_bp)
