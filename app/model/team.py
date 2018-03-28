bd0a94d4de0dc5d83729035659d4670a61baaf24
zwkatgithub <zhangwenkai2013@163.com>
2018-03-28 09:39:15 +0800

    Update team.py

--- app/model/team.py
+++ app/model/team.py
@@ -7,7 +7,7 @@ class PlayerBase(db.Model):
 
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(50))
-    birthday = db.Column(db.DateTime)
+    birthday = db.Column(db.Date)
     country = db.Column(db.String(45))
     height = db.Column(db.Float)
     weight = db.Column(db.Float)
@@ -15,13 +15,25 @@ class PlayerBase(db.Model):
     reach_height = db.Column(db.Float)
     draft = db.Column(db.String(255))
     contract = db.Column(db.String(255))
-    season_id = db.Column(db.Integer)
-    team_id = db.Column(db.Integer)
+    season_data_id = db.Column(db.Integer, db.ForeignKey('season_data.id'))
+    team_id = db.Column(db.Integer, db.ForeignKey("team_info.id"))
     cloth_num = db.Column(db.Integer)
     pos1 = db.Column(db.String(2))
     pos2 = db.Column(db.String(2))
     price = db.Column(db.Integer)
     score = db.Column(db.Integer)
+    
+    season = db.relationship('SeasonData',backref='playerbase',lazy='dynamic')
+    team = db.relationship("TeamInfo",backref='playerbase',lazy='dynamic')
+    
+    def __init__(self, name, birthday, country, height, wieght, armspan,
+                 reach_height, draft, contract, season_id,team_id,cloth_num,pos1,pos2,price,score):
+        (self.name, self.birthday, self.country, self.height, self.wieght, self.armspan, self.reach_height, self.draft,
+         self.contract, self.season_data_id, self.team_id, self.cloth_num, self.pos1, self.pos2,self.price, self.score )=
+        (name, birthday, country, height, wieght, armspan,
+        reach_height, draft, contract, season_id,team_id,cloth_num,pos1,pos2,price,score)
+    def __repr__(self):
+        return "<PlayerBase %r>" % self.id
 
 
 # 球队信息
@@ -32,3 +44,9 @@ class TeamInfo(db.Model):
     name = db.Column(db.String(255))
     city = db.Column(db.String(45))
     intro = db.Column(db.String(255))
+    
+    def __init__(self, name, city, intro):
+        self.name, self.city, self.intro = (
+            name, city, intro)
+    def __repr__(self):
+        return "<TeamInfo %r>" % self.id
