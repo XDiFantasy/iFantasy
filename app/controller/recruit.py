df533b2ba67a39e85311865a4ce2f492c46ba039
sonp <sonpking@gmail.com>
2018-03-28 21:01:39 +0800

    first

--- app/controller/recruit.py
+++ app/controller/recruit.py
@@ -5,3 +5,9 @@ recruit_bp = Blueprint('recruit_bp', __name__)
 recruit_api = Api(recruit_bp)
 
 # your code
+
+class hello(Resource):
+    def get(self):
+        return "hello"
+
+recruit_api.add_resource(hello,'/')
