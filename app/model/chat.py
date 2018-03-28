b490ab5e9e883cecfa67662f818493c678398067
zwkatgithub <zhangwenkai2013@163.com>
2018-03-28 09:57:19 +0800

    Create chat.py

---
+++ app/model/chat.py
@@ -0,0 +1,17 @@
+class Friend(db.Model):
+    __tablename__ = "friend"
+    __table_args__ = (
+        db.PrimaryKeyConstraint('user_id', 'friend_id'),
+    )
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    friend_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    user = db.relationship('User', backref='friend', lazy='dynamic')
+
+    def __init__(self, user_id, friend_id):
+        self.user_id, self.friend_id = (
+            user_id, friend_id
+        )
+
+    def __repr__(self):
+        return "<Friend %r, %r>" % (self.user_id, self.friend_id)
