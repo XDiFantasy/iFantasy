b1c27c67267b749c0f63b5b048ced2922b19d96e
xd-sonp <sonpking@163.com>
2018-03-29 17:19:20 +0800

    add recruit model

--- app/controller/recruit.py
+++ app/controller/recruit.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
-from flask_restful import abort, Api, Resource
-from flask.ext.restful import fields, marshal_with, reqparse
+from flask_restful import abort, Api, Resource, fields, marshal_with, reqparse
+from app import db
+from app.model import Recruit
 
 recruit_bp = Blueprint('recruit_bp', __name__)
 recruit_api = Api(recruit_bp)
@@ -10,3 +11,16 @@ def abort_if(todo_id):
     if False:
         abort(404, message="".format())
 
+
+
+class GetRecruit(Resource):
+    def get(self)):
+        parser.add_argument()
+        db.session.query(Recruit).filter_by(name='ed').first()
+
+
+recruit_api.add_resource(GetRecruit,'/get_recruit_info')
+recruit_api.add_resource(OneRecruit,'/one_recruit')
+recruit_api.add_resource(FiveRecruie,'/five_recruit')
+recruit_api.add_resource(RecruitPlayer,'/recruit')
+recruit_api.add_resource(ShowPlayer,'/show_all_payer')
