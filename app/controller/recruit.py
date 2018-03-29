593cb01dc7f9983a75f3395303c78ad22d012212
xd-sonp <sonpking@163.com>
2018-03-29 10:26:21 +0800

    add name

--- app/controller/recruit.py
+++ app/controller/recruit.py
@@ -1,38 +1,15 @@
 from flask import Blueprint
-from flask_restful import reqparse, abort, Api, Resource
-from flask import jsonify
+from flask_restful import abort, Api, Resource
+from flask.ext.restful import fields, marshal_with, reqparse
 
 recruit_bp = Blueprint('recruit_bp', __name__)
 recruit_api = Api(recruit_bp)
-
-# your code
-TODOS = {
-    'todo1': {'task': 'build an API'},
-    'todo2': {'task': '?????'},
-    'todo3': {'task': 'profit!'},
-}
-
 parser = reqparse.RequestParser()
-parser.add_argument('task', type=str)
 
 def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
+    if False:
+        abort(404, message="".format())
 
-class Todo(Resource):
-    def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
 
-    def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
-        return '', 204
-
-    def put(self, todo_id):
-        args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
-        return task, 201
 
 recruit_api.add_resource(Todo, '/todos/<todo_id>')
