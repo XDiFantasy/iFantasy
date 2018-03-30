38267478388a4ee418b2df5b59637a611a441daf
xiaolinzi <daoshangxl@163.com>
2018-03-30 19:51:29 +0800

    本地下载

--- app/model/__init__.py
+++ app/model/__init__.py
@@ -1,7 +1,7 @@
 from .activity import Theme, Vip, VipCard, Fund, FundType
-from .bag import BagEquip, BagPiece, BagPlayer, BagProp, PropUsing, InputData,Equip,Piece,BagTrailCard
+from .bag import BagEquip, BagPiece,  BagProp, PropUsing, InputData,Equip,Piece,BagTrailCard
 from .chat import Friend
 from .game import GameHistory, UserGame
 from .tactics import Strategy, AttrCh
-from .team import PlayerBase,TeamInfo, SeasonData
-from .user import User, LineUp
+from .team import PlayerBase,TeamInfo, SeasonData,LineUp,BagPlayer
+from .user import User
