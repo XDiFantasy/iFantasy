ff6d1ee49f0052b047da77f788dac479b2796a6b
xiaolinzi <daoshangxl@163.com>
2018-03-30 22:54:40 +0800

    修改contract

--- app/model/__init__.py
+++ app/model/__init__.py
@@ -1,17 +1,17 @@
 from .activity import Theme, Vip, VipCard, Fund, FundType
-# <<<<<<< HEAD
+<<<<<<< HEAD
 from .bag import BagEquip, BagPiece,  BagProp, PropUsing, InputData,Equip,Piece,BagTrailCard
 # =======
 from .bag import BagEquip, BagPiece, BagProp, PropUsing, InputData,Equip,Piece,BagTrailCard
-# >>>>>>> 8f18148866463f737d83e9e6f47ae47bb091232f
+>>>>>>> 8f18148866463f737d83e9e6f47ae47bb091232f
 from .chat import Friend
 from .game import UserGame
 from .tactics import Strategy, AttrCh
-# <<<<<<< HEAD
+<<<<<<< HEAD
 from .team import PlayerBase,TeamInfo, SeasonData,LineUp,BagPlayer
 from .user import User
-# =======
+=======
 from .team import PlayerBase,TeamInfo, SeasonData, BagPlayer, LineUp
 from .user import User
 from .recruit import Recruit
-# >>>>>>> 8f18148866463f737d83e9e6f47ae47bb091232f
+>>>>>>> 8f18148866463f737d83e9e6f47ae47bb091232f
