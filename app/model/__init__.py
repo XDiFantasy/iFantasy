7ad31c17395bb127f4397c9fe344f1f6f69ee840
zwkatgithub <zhangwenkai2013@163.com>
2018-03-30 11:44:51 +0800

    Update __init__.py

--- app/model/__init__.py
+++ app/model/__init__.py
@@ -1,8 +1,8 @@
 from .activity import Theme, Vip, VipCard, Fund, FundType
-from .bag import BagEquip, BagPiece, BagPlayer, BagProp, PropUsing, InputData,Equip,Piece,BagTrailCard
+from .bag import BagEquip, BagPiece, BagProp, PropUsing, InputData,Equip,Piece,BagTrailCard
 from .chat import Friend
-from .game import GameHistory, UserGame
+from .game import UserGame
 from .tactics import Strategy, AttrCh
-from .team import PlayerBase,TeamInfo, SeasonData
+from .team import PlayerBase,TeamInfo, SeasonData, BagPlayer
 from .user import User, LineUp
 from .recruit import Recruit
