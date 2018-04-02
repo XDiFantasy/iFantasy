f0801bd200808b7b5146acbe730b1940391cb469
zwkatgithub <zhangwenkai2013@163.com>
2018-04-02 09:37:03 +0800

    Update __init__.py

--- app/controller/__init__.py
+++ app/controller/__init__.py
@@ -1,4 +1,5 @@
 # from app.controller.activity import activity_bp
+from .message import Message
 from .chat import chat_bp
 from .game import game_bp, GameError
 from .recruit import recruit_bp
@@ -7,4 +8,3 @@ from .team import team_bp
 from .user import user_bp,Auth,UserError
 from .activity import activity_bp
 from .bag import bag_bp
-from .message import Message
